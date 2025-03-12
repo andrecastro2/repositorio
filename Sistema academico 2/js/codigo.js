2304cbd397c4577fb4a2f7c826a9da914dbef1e3
andrecastro2 <andrechimp@gmail.com>
2025-03-11 21:40:14 -0300

    Add files via upload

---
+++ Sistema academico 2/js/codigo.js
@@ -0,0 +1,30 @@
+
+function testa_form(){
+   var aluno = document.forms ["formalunos"]["aluno"].value;
+
+   var cpf = document.forms ["formalunos"]["cpf"].value;
+
+   //verifica se o campo "aluno" esta vazio
+
+   if (aluno == ""){
+         alert("Nome do aluno precisa ser informado");
+
+         document.forms["formalunos"]["aluno"].focus();
+        return false;}
+
+        //verifica se o campo "cpf" esta vazio
+
+   if (cpf == ""){
+            alert("cpf do aluno precisa ser informado");
+
+            document.forms["formalunos"]["cpf"].focus();
+
+           return false;}
+
+
+   }
+
+
+
+
+
